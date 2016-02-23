3b7890daa6ab570fa3c150e67ed328da47ef87e0
readpan <readpan@gmail.com>
2016-02-23 15:53:06 +0800

    Initial FSM

---
+++ Assets/FSM/IState.cs
@@ -0,0 +1,18 @@
+﻿public interface IState
+{
+    /// <summary>
+    /// 获取这个状态机的状态
+    /// </summary>
+    /// <returns></returns>
+    uint GetStateID();
+
+    void OnEnter();
+
+    void OnExit();
+
+    void OnUpdate();
+
+    void OnFixedUpdate();
+
+    void OnLateUpdate();
+}
